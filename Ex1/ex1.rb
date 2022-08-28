9ef3d02d59fba022278b68a8a52a8c67d082b922
khalongtvh <khalongtvh@gmail.com>
2022-08-28 08:56:50 +0700

    first commit

---
+++ Ex1/ex1.rb
@@ -0,0 +1,20 @@
+# bài 1
+# name = "Trần Văn Khánh"
+# puts "Xin chào " + name
+
+#  bài 2
+# puts "Nhập số cần in"
+# num = gets
+# puts num
+
+# bài 3
+print "Nhập số thứ nhất : "
+num1 = gets.to_f
+print "Nhập số thứ hai : "
+num2 = gets.to_f
+print "Số lớn hơn : "
+if num1 > num2
+  print num1
+else
+  print num2
+end
